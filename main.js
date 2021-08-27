81c41d0551f6c6069487bf50d90dc292903c73db
Pranay Katakam <pkataka1@uncc.edu>
2021-08-26 20:29:27 -0400

    Pranay|Added node js server

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-console.log("Hello world")
+const http = require('http');
 
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('ITIS week 1 Assignment');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
+console.log("Hello world");
